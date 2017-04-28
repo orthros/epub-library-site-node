4d75d9cc29584c7b71fa24f764eeaebb45f79904
Colin <colinnlsn@gmail.com>
2017-04-28 11:22:09 -0500

    Added Pug
    Added Pug and Aample pug index.pug
    Express will now use pug as the view engine

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const pug = require('pug');
+
 var express = require('express')
 var app = express()
 
+app.set('view engine', 'pug')
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
